87e904a8eb42fe70e18f8846d1e6c11db0ba6bbc
Jonathan Sweemer <sweemer@gmail.com>
2011-09-19 14:13:49 +0900

    Adding init file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name              = "pamfax",
     version           = "0.0.1",
     url               = "http://github.com/dynaptico/pamfaxp",
-    maintainer        = "Dynaptico LLC",
+    maintainer        = "Dynaptico LLC (http://www.dynaptico.com)",
     maintainer_email  = "support@dynaptico.com",
     description       = "Python implementation of the PamFax API",
     long_description  = "This module implements a set of classes and methods for sending and receiving faxes via the PamFax API using the Python programming language",

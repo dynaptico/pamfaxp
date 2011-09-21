da58df196f33f7940f64234e7adbc59e2efd103b
Jonathan Sweemer <sweemer@gmail.com>
2011-09-21 11:51:20 +0900

    Bumped up major version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name              = "dynaptico-pamfax",
-    version           = "0.0.9",
+    version           = "1.0.0",
     url               = "http://github.com/dynaptico/pamfaxp",
     maintainer        = "Dynaptico LLC (http://www.dynaptico.com)",
     maintainer_email  = "support@dynaptico.com",
